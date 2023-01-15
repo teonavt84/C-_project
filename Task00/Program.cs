99ba26da9776a304e47804e8cc7841c1cd1d945c
Denis <denis.volobuev1984@yandex.ru>
2023-01-15 15:03:39 +0300

    Add Task01

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат.
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("Enter number 1: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberTwo * numberTwo == numberOne)
+{
+    Console.WriteLine($"{numberOne} является квадратом {numberTwo}");
+}
+else
+{
+    Console.WriteLine($"{numberOne} не является квадратом {numberTwo}");
+}
