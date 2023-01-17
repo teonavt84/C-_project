49ed39e5aa2ba479f7467b07627bbb46855da933
Denis <denis.volobuev1984@yandex.ru>
2023-01-17 18:26:47 +0300

    Correction Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,19 +1,20 @@
 ﻿// Задача 8. Напишите программу, которая на входе принимает число (N), а на выходе показывает все четные числа от 1 до N.
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int i = 2;
-if (number == 1)
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (num < 0)
+    {
+        Console.WriteLine("Введенное число должно быть целым.");
+    }
+if (num == 1)
+    {
+        Console.WriteLine("У этого числа нет четных чисел.");
+    }
+while(i <= num)
 {
-    Console.WriteLine("У этого числа нет четных чисел.");
-}
-else
-if (number > 0)
-    while (i < number)
+    if (i % 2 == 0)
     {
         Console.Write(i + ", ");
-        i += 2;
     }
-else
-{
-    Console.WriteLine("Введенное число должно быть больше нуля");
-}
+        i++;
+}    
