2accbdd64bab6e796b4430626060627df722af85
Denis <denis.volobuev1984@yandex.ru>
2023-01-21 20:50:25 +0300

    Add Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -3,16 +3,27 @@
 // квадратов чисел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// void TabSquare(int number)
+// {
+//     int count = 1;
+//     while (count <= number)
+//     {
+//         Console.WriteLine($"{count} {count * count,5}");
+//         count++;
+//     }
+// }
+// Console.WriteLine($"Таблица квадратов для числа {num}:");
+// TabSquare(num);
+
+// Через цикл for
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 void TabSquare(int number)
 {
-    int count = 1;
-    while (count <= number)
-    {
-        Console.WriteLine($"{count} {count * count}");
-        count++;
-    }
+    for (int i = 1; i <= num; i++)
+        Console.WriteLine($"{i} {i * i,5}");
 }
 Console.WriteLine($"Таблица квадратов для числа {num}:");
 TabSquare(num);
